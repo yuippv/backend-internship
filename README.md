6190925c3c7c71368b9766c75e2c74f2951eaf22
Noppawit Suttichet <taemanitai@gmail.com>
2021-06-01 13:03:27 +0700

    [INIT] 🔥 template project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # intern-backend
+
+basic nodejs express template
+
+run npm run start or node app.js or yarn start
+
+Example app listening at http://localhost:8080
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// app.post('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.put('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.delete('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
