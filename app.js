769396378c0196d52a733604b8ab66206244559e
nattawatsupangsarn015 <superpower-max@hotmail.com>
2021-06-04 11:08:03 +0700

    [ADD]✨ add schema and connection local database

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/test/:id/:count", (req, res) => {
+  console.log({ param: req.params });
+  console.log({ query: req.query });
+  res.send({ head: "Hello world" });
+});
 
-app.get('/', (req, res) => {
-  console.log('req: ', req.query);
-  res.send('Hello World!')
-})
+app.post("/demo", (req, res) => {
+  console.log("req: ", req.body);
+  res.send("Hello World!");
+});
 
-app.post('/demo', (req, res) => {  
-  console.log('req: ', req.body);
-  res.send('Hello World!')
-})
+app.put("/", (req, res) => {
+  console.log("req: ", req.body);
+  res.send("Hello World!");
+});
 
-app.put('/', (req, res) => {
-  console.log('req: ', req.body);
-  res.send('Hello World!')
-})
-
-app.delete('/', (req, res) => {
-  console.log('req: ', req.body);
-  res.send('Hello World!')
-})
+app.delete("/", (req, res) => {
+  console.log("req: ", req.body);
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
