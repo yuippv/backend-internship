062128e1db6f4029d34ed9615cad50cd3c744690
Noppawit Suttichet <taemanitai@gmail.com>
2021-06-04 12:05:44 +0700

    [EDIT] 🧪 update create function

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(connectMongo);
 
+// findbook
 app.get("/book/:name", async (req, res) => {
   // console.log({ param: req.params });
   // console.log({ query: req.query });
@@ -20,17 +21,26 @@ app.get("/book/:name", async (req, res) => {
   res.send(bookData);
 });
 
+//create book
 app.post("/create/book", async (req, res) => {
-  console.log("req: ", req.body);
-  const book = await crateBook(req.body);
-  res.send(book);
+  try{
+    console.log("req: ", req.body);
+    const book = await crateBook(req.body);
+    res.send(book);
+  }
+  catch(err){
+    console.log('err: ', err);
+    res.send(err);
+  }
 });
 
+//update book
 app.put("/", (req, res) => {
   console.log("req: ", req.body);
   res.send("Hello World!");
 });
 
+//delete book
 app.delete("/", (req, res) => {
   console.log("req: ", req.body);
   res.send("Hello World!");
--- src/functions/index.js
+++ src/functions/index.js
@@ -3,13 +3,17 @@ const BookModel = require('../models/books.model')
 
 module.exports.crateBook = async (input) => {
     console.log('input: ', input);
+    const { name, price } = input;
     // validate data
+    if(!name){
+        throw { message: 'no name'}
+    }
 
     // name: { type: String, default: "-" },
     // price: { type: Number },
 
     // create data
-    return BookModel.create(input);
+    return BookModel.create({ name, price });
 }
 
 
