dca7ed810af0883b2192ffcfcb6369a3c6e0c900
Noppawit Suttichet <taemanitai@gmail.com>
2021-06-04 10:39:57 +0700

    [ADD] 🔨 uncomment route and add package

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
 app.get('/', (req, res) => {
+  console.log('req: ', req.query);
   res.send('Hello World!')
 })
 
-// app.post('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.post('/demo', (req, res) => {  
+  console.log('req: ', req.body);
+  res.send('Hello World!')
+})
 
-// app.put('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.put('/', (req, res) => {
+  console.log('req: ', req.body);
+  res.send('Hello World!')
+})
 
-// app.delete('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.delete('/', (req, res) => {
+  console.log('req: ', req.body);
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
