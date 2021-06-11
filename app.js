23a541542e507a8b04cb2808517f2983319bdbd3
JoJo <titichaya_love@hotmail.com>
2021-06-11 16:48:07 +0700

    [ADD] 🥓 CRUD

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const {
   createUser,
+  findUserById,
+  updateUserById,
+  deleteUserById
 
 } = require("./src/functions/index");
 const connectToDatabase = require("./src/utils/mongo");
@@ -22,9 +25,44 @@ app.post("/user", async (req, res) => {
     const user = await createUser(req.body);
     res.send(user);
   } catch (err) {
+    console.log("err: ", err);
     res.status(err.status || 500).send(err.message || "Internal Server Error");
   }
 });
+
+// find user by id
+app.get("/user/:id", async (req, res) => {
+  try {
+    const userData = await findUserById(req.params.id);
+    res.send(userData);
+  } catch (err) {
+    console.log("err: ", err);
+    res.status(err.status || 500).send(err.message || "Internal Server Error");
+  }
+});
+
+//update user by id
+app.put("/user/:_id", async (req, res) => {
+  try {
+    const updateUser = await updateUserById(req.body, req.params._id);
+    res.send(updateUser);
+  } catch (err) {
+    console.log("err:", err);
+    res.status(err.status || 500).send(err.message || "Internal Server Error");
+  }
+})
+
+app.delete("/user/:_id", async (req, res) => {
+  try {
+    const deleteUser = await deleteUserById(req.params._id);
+    res.send(deleteUser);
+  } catch (err) {
+    console.log("err:", err);
+    res.status(err.status || 500).send(err.message || "Internal Server Error");
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- src/functions/index.js
+++ src/functions/index.js
@@ -3,7 +3,7 @@ const UserModel = require("../models/user.model");
 var mongoose = require('mongoose');
 
 module.exports.createUser = async (input) => {
-  const { name, lastname, username, email, password, image } = input;
+  const { name, lastname, username, email, password, image, isDeleted } = input;
 
   if (!name) {
     throw { message: "no name" };
@@ -21,6 +21,72 @@ module.exports.createUser = async (input) => {
     throw { message: "no password" };
   }
 
-  return await UserModel.create({ name, lastname, username, email, password, image });
+  return await UserModel.create({ name, lastname, username, email, password, image, isDeleted });
+
+}
+
+module.exports.findUserById = async (input) => {
+  if (mongoose.Types.ObjectId.isValid(input)) {
+    return await UserModel.findOne({ _id: input, isDeleted: false });
+  }
+  else {
+    throw {
+      message: "user not found",
+      status: 404
+    };
+  }
+}
+
+module.exports.updateUserById = async (payload, userId) => {
+  const { name, lastname, username, email, password, image } = payload;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    throw {
+      message: "Invalid ID",
+      status: 404
+    };
+  }
+
+  const user = await UserModel.findOne({
+    _id: userId,
+    isDeleted: false
+  });
+
+  if (!user) {
+    throw { message: "user not found", status: 404 };
+  }
+
+  return UserModel.findOneAndUpdate(
+    { _id: userId },
+    { name, lastname, username, email, password, image },
+    { new: true, omitUndefined: true }
+  )
+
+};
+
+module.exports.deleteUserById = async (userId) => {
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    throw {
+      message: "Invalid ID",
+      status: 404
+    };
+  }
+
+  const checkDelete = await UserModel.findOne({
+    _id: userId,
+    isDeleted: true
+  });
+
+  if (checkDelete) {
+    throw { message: "This user is already deleted" }
+  }
+
+  return UserModel.findOneAndUpdate(
+    { _id: userId },
+    {
+      delete_at: new Date(),
+      isDeleted: true
+    },
+    { new: true }
+  )
 
 }
