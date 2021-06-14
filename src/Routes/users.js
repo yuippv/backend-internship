2c46d8efbe5af3b6d381cef449e78888a10cbb77
JoJo <titichaya_love@hotmail.com>
2021-06-14 16:52:45 +0700

    [ADD] 🔨 post, get content route

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -1,16 +1,17 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/usersController')
+const usersController = require("../controllers/usersController");
 
+router.post("/user", usersController.createUsers);
+router.get("/user/:id", usersController.findUserById);
+router.put("/user/:_id", usersController.updateUserById);
+router.delete("/user/:_id", usersController.deleteUserById);
+router.post("/user/create/result/:id", usersController.createResultById);
+router.get("/user", usersController.getAllUsers);
+router.post("/comment", usersController.postComment);
+router.post("/guest", usersController.createGuest);
+router.get("/user/result/:id", usersController.getResultById);
+router.post("/user/content", usersController.postContent);
+router.get("/user/content/get", usersController.getAllContents);
 
-router.post("/user",usersController.createUsers)
-router.get("/user/:id",usersController.findUserById)
-router.put("/user/:_id",usersController.updateUserById)
-router.delete("/user/:_id",usersController.deleteUserById)
-router.post("/user/create/result/:id",usersController.createResultById)
-router.get("/user",usersController.getAllUsers)
-router.post("/comment",usersController.postComment)
-router.post("/guest",usersController.createGuest)
-router.get("/user/result/:id",usersController.getResultById)
-
-module.exports = router
+module.exports = router;
