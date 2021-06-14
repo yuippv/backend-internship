42c83afd9bc0c3319b259f418de14a46c3534aeb
jasonnonstop10 <jay44411@gmail.com>
2021-06-14 14:30:58 +0700

    [FIX] fixLogin

--- app.js
+++ app.js
@@ -3,89 +3,21 @@ require('./src/middlewares/index');
 const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-const {
-  createUser,
-  findUserById,
-  updateUserById,
-  deleteUserById,
-  createResultById,
-  getResultById,
-} = require("./src/functions/index");
 
 const connectToDatabase = require("./src/utils/mongo");
 const app = express();
 const port = 5000;
 
+const connectMongo = async (req, res, next) => {
+  await connectToDatabase();
+  next();
+};
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-connectToDatabase();
+app.use(connectMongo);
 
-//create user
-app.post("/user", async (req, res) => {
-  try {
-    const user = await createUser(req.body);
-    res.send(user);
-  } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
-
-// find user by id
-app.get("/user/:id", async (req, res) => {
-  try {
-    const userData = await findUserById(req.params.id);
-    res.send(userData);
-  } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
-
-//update user by id
-app.put("/user/:_id", async (req, res) => {
-  try {
-    const updateUser = await updateUserById(req.body, req.params._id);
-    res.send(updateUser);
-  } catch (err) {
-    console.log("err:", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
-
-app.delete("/user/:_id", async (req, res) => {
-  try {
-    const deleteUser = await deleteUserById(req.params._id);
-    res.send(deleteUser);
-  } catch (err) {
-    console.log("err:", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
-app.post("/user/create/result/:id", async (req, res) => {
-  try {
-    const userid = req.params.id;
-    const description = req.body.description;
-    const result = req.body.result;
-    const score = req.body.score;
-    const user = await createResultById(description, result, score, userid);
-    res.send(user);
-  } catch (err) {
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
-
-app.get("/user/result/:id", async (req, res) => {
-  try {
-    const userid = req.params.id;
-
-    const user = await getResultById(userid);
-    res.send(user);
-  } catch (err) {
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
-  }
-});
 
 //sign up
 app.post(
@@ -101,19 +33,19 @@ app.post(
 
 //log in
 app.post("/login", async (req, res, next) => {
-  passport.authenticate("login", async (err, auth, info) => {
+  passport.authenticate("login", async (err, auths, info) => {
     try {
-      if (err || !auth) {
+      if (err || !auths) {
         const error = new Error("An error occurred.");
 
         return next(error);
       }
 
-      req.login(auth, { session: false }, async (error) => {
+      req.login(auths, { session: false }, async (error) => {
         if (error) return next(error);
 
-        const body = { _id: auth._id, email: auth.AID };
-        const token = jwt.sign({ auth: body }, "TOP_SECRET");
+        const body = { _id: auths._id, email: auths.AID };
+        const token = jwt.sign({ auths: body }, "TOP_SECRET");
 
         return res.json({ token });
       });
