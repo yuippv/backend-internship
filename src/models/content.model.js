5744ed48858c8440bd3c5b0d576db00db9471bc4
JoJo <titichaya_love@hotmail.com>
2021-06-14 16:52:08 +0700

    [ADD] 🔧 content model

---
+++ src/models/content.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contents = new Schema(
+  {
+    content_body: { type: String, default: "-" },
+    title: { type: String, default: "-" },
+    likes: { type: Number, default: 0 },
+    uid_likes: [{ type: String, default: "-" }],
+    comment_id: [{ type: String, default: "-" }],
+    author_name: { type: String, default: "-" },
+    author_id: { type: String, defalut: "-" },
+    tag: { type: String, default: "-" },
+    image: { type: String, default: "-" },
+    isDeleted: { type: Boolean, default: false }
+  },
+  {
+    strict: false,
+    versionKey: false,
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+module.exports = mongoose.model("contents", contents);
