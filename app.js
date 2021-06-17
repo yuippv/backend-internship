812e16976bc2cf02061c7c044efff03a70dc5e95
chanok <Chanok_boon@hotmail.com>
2021-06-17 15:49:11 +0700

    FIX BUG

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ app.use(connectMongo);
 app.use(userRoutes)
 app.use(adminRoute)
 
+app.use("/", authRoutes);
+//Buffer better
+app.post(
+  "/images/:userId",
+  multer({
+    dest: "uploads/",
+  }).array("photo", 10),
+  async (req, res) => {
+    //J calling
+    const userId = req.params.userId
+    const file = req.files
+    const result = await uploadManyFile(file,userId,"userResult")
+    console.log(result);
+    res.send(result);
+  }
+);
+app.use(userRoutes);
+app.use(adminRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/utils/s3.js
@@ -0,0 +1,78 @@
+
+
+const path = require("path");
+const fs = require('fs')
+const aws = require("aws-sdk");
+const {AWS_BUCKET_NAME} = process.env
+
+const bucketRegion  = process.env.AWS_BUCKET_REGION 
+const bucketKey     =  process.env.AWS_ACCESS_KEY 
+const bucket_secret = process.env.SECRET_ACCESS_KEY 
+
+
+
+aws.config.setPromisesDependency();
+aws.config.update({
+  accessKeyId: bucketKey,
+  secretAccessKey: bucket_secret,
+  region: bucketRegion,
+  
+});
+
+const s3  = new aws.S3()
+//upload a file to s3 
+// async function uploadFile(file,userId)  {
+//     const fileStream = fs.createReadStream(file.path)
+//     const uploadParams = {
+//         Bucket: bucketName,
+//         Body: fileStream,
+//         Key: "userAvatar/" + userId,
+//         ContentType:  "image/jpeg",
+//         ACL: "public-read",
+//     }
+    
+    
+//     return s3.upload(uploadParams).promise()
+
+// }
+async function  uploadManyFile(files,userId,pathS3){
+  const fileReturn = await Promise.all(
+    files.map(async (item, index) => {
+      const filePath = path.join(__dirname, "..","..", "uploads", item.filename);
+      const key = pathS3 + "/" + userId + "/" + item.filename + '.jpeg'
+     
+      var params = {
+        Bucket: AWS_BUCKET_NAME,
+        Key: key,
+        Body: fs.createReadStream(filePath),
+        ContentType: "image/jpeg",
+        ACL: "public-read",
+      };
+  
+      const data = await s3
+        .upload(params)
+        .promise()
+        .then((data) => {
+          fs.unlinkSync(filePath);
+  
+          return data;
+        });
+  
+      return data["Location"];
+    })
+  );
+  
+  return fileReturn;
+
+
+
+}
+
+
+
+
+
+exports.uploadManyFile = uploadManyFile
+
+
+//dowmload a file from 23
