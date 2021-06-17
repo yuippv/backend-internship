aadf4587a1fdbaf2f29571d92d6140104d5ce294
jasonnonstop10 <jay44411@gmail.com>
2021-06-17 14:09:32 +0700

    [UPDATE] update role in auth

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -3,17 +3,29 @@ const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
-const AuthSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const AuthSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: [user, admin],
+      default: "admin",
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    strict: false,
+    versionKey: false,
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
 
 //hash password
 AuthSchema.pre("save", async function (next) {
