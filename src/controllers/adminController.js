5f1c9e13d5b3c287f4155cada31ec0123863c667
JoJo <titichaya_love@hotmail.com>
2021-06-22 15:15:24 +0700

    [ADD] exception handling to every admin fuctions

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,64 +1,97 @@
 const questionModel = require("../models/questions.model");
-const { findAdminById, findAllAdmins } = require("../functions/index");
+const {
+  findAdminById,
+  findAllAdmins,
+  postQuestion,
+} = require("../functions/index");
 const resultor = require("../models/result.model");
 
 exports.getAllResult = async (req, res) => {
   try {
     const results = await resultor.find();
-    res.send(results);
+    if (!results.length) {
+      throw {
+        message: "result not found",
+        status: 404,
+      };
+    } else res.send(results);
   } catch (err) {
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
   }
 };
 
-exports.getAdminById = async (req, res) => {
+exports.getAdminById = async (req, res, next) => {
   try {
-    const { userId } = req;
-    const admin = await findAdminById(userId);
-    res.send(admin);
+    if (req.body._id) {
+      const userId = req.body._id;
+      const admin = await findAdminById(userId);
+      res.send(admin);
+    } else {
+      const { userId } = req;
+      const admin = await findAdminById(userId);
+      res.send(admin);
+    }
   } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
   }
 };
 
-exports.getAllAdmins = async (req, res) => {
+exports.getAllAdmins = async (req, res, next) => {
   try {
     const admins = await findAllAdmins();
     res.send(admins);
   } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
   }
 };
 
-exports.postQuestion = async (req, res) => {
+exports.postQuestion = async (req, res, next) => {
   try {
-    const question = await questionModel.create(req.body);
+    const question = await postQuestion(req.body);
     res.send(question);
   } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
   }
 };
 
-exports.getQuestions = async (res) => {
+exports.getAllQuestions = async (req, res, next) => {
   try {
-    const question = await questionModel.find();
-    res.send(question);
+    const question = await questionModel.find({});
+    if (!question.length) {
+      throw {
+        message: "question not found",
+        status: 404,
+      };
+    } else res.send(question);
   } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
   }
 };
 
-exports.getQuestionByCat = async (req, res) => {
+exports.getQuestionByCat = async (req, res, next) => {
   try {
     const catName = req.body.question_category;
     const question = await questionModel.find({ question_category: catName });
+    if (!question.length) {
+      throw {
+        message: "Invalid category",
+        status: 400,
+      };
+    }
     res.send(question);
   } catch (err) {
-    console.log("err: ", err);
-    res.status(err.status || 500).send(err.message || "Internal Server Error");
+    next(err);
+  }
+};
+
+exports.updateFields = async (req, res, next) => {
+  try {
+    const up = await questionModel.updateMany(
+      {},
+      { $rename: { QCAT: "question_category" } }
+    );
+    res.send(up);
+  } catch (err) {
+    next(err);
   }
 };
