563eec9a3b2d1eda770928847bdf302d6ceecd11
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-07-05 17:04:00 +0700

    [edit] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 pm2 stop all ; 
 pm2 delete all ; 
 git pull ; 
+rm -rf .env
 touch .env;
 echo 'DB_CONNECTION=mongodb+srv://vonderintern21:vonderintern112@cluster0.7v210.mongodb.net/test' >> .env
 echo 'Secret_Key = "secretApp"' >> .env
