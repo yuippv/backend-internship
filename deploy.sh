3da827d76000633f86d5aa9d8104581183ceb5db
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-07-06 12:39:25 +0700

    [edit] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ pm2 delete all ;
 git pull ; 
 rm -rf .env
 touch .env;
-echo ${DB} >> .env
-echo ${Secret_Key} >> .env
+echo '${DB}' >> .env
+echo '${Secret_Key}' >> .env
 npm install ; 
 pm2 start "npm start" ;
