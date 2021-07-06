0f3b5bfeec1fbef653a4de401f9a3db913ae7a1f
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-07-06 12:36:57 +0700

    [edit] config.yml,deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ pm2 delete all ;
 git pull ; 
 rm -rf .env
 touch .env;
-echo 'DB_CONNECTION=mongodb+srv://vonderintern21:vonderintern112@cluster0.7v210.mongodb.net/test' >> .env
-echo 'Secret_Key = "secretApp"' >> .env
+echo ${DB} >> .env
+echo ${Secret_Key} >> .env
 npm install ; 
 pm2 start "npm start" ;
