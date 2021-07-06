1d34f28fe77bdeaa3941985c144f84739e90d3f3
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-07-06 13:54:34 +0700

    [edit] config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 pm2 stop all  
 pm2 delete all  
 git pull  
-rm -rf .env
-touch .env
-echo "DB_CONNECTION="${DB}"" >> .env
-echo "Secret_Key=".circleci/config.yml/$SK"" >> .env
+# rm -rf .env
+# touch .env
+# echo "DB_CONNECTION="${DB}"" >> .env
+# echo "Secret_Key="$SK"" >> .env
 npm install 
 pm2 start "npm start" 
